9a7fe1126f4f6106743ce6196ee2905b8b083fc2
Karen Palacio <karen.palacio.1994@gmail.com>
2023-11-09 00:03:32 -0500

    hacer que ande busqueda y similitudes

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+window.addEventListener('load', () => {
+    // Fetch descriptions from the file_list.txt file
+    fetchImageDescriptions()
+        .then(descriptions => {
+            const commonWords = analyzeCommonWords(descriptions);
+            displayCommonWords(commonWords);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
+
 document.getElementById('searchButton').addEventListener('click', function () {
     const searchInput = document.getElementById('searchInput');
     const searchTerms = searchInput.value.toLowerCase().trim();
@@ -14,7 +26,7 @@ document.getElementById('searchButton').addEventListener('click', function () {
     fetchImageDescriptions()
         .then(descriptions => {
             // Filter and display images based on search terms
-            const matchingImages = filterImagesByDescription(descriptions, searchTerms);
+            const matchingImages = findImagesWithSimilarWords(descriptions, searchTerms);
             displayImages(matchingImages);
         })
         .catch(error => {
@@ -31,28 +43,96 @@ async function fetchImageDescriptions() {
     }
 
     const descriptions = await response.text();
-    return descriptions.split('\n');
+    return descriptions;
 }
 
-// Function to filter images based on description
-function filterImagesByDescription(descriptions, searchTerms) {
-    return descriptions.filter(description => {
-        return description.toLowerCase().includes(searchTerms);
+// Function to analyze common words using nlp-compromise
+function analyzeCommonWords(text) {
+    const nlp = window.nlp;
+
+    // Parse the text with nlp-compromise
+    const doc = nlp(text);
+
+    // Get the most common terms
+    const terms = doc.terms().out('array');
+
+    // Count term occurrences
+    const termCount = terms.reduce((count, term) => {
+        count[term] = (count[term] || 0) + 1;
+        return count;
+    }, {});
+
+    // Sort terms by frequency
+    const sortedTerms = Object.entries(termCount)
+        .sort((a, b) => b[1] - a[1])
+        .slice(0, 10); // Display the top 10 terms
+
+    return sortedTerms;
+}
+
+// Function to display common words
+function displayCommonWords(commonWords) {
+    const commonWordsContainer = document.getElementById('commonWordsContainer');
+    commonWordsContainer.innerHTML = '<h3>Most Common Words:</h3>';
+    
+    const ul = document.createElement('ul');
+    commonWords.forEach(([term, count]) => {
+        const li = document.createElement('li');
+        li.textContent = `${term} (${count} occurrences)`;
+        ul.appendChild(li);
     });
+
+    commonWordsContainer.appendChild(ul);
+}
+
+// Function to find images with similar descriptions using Jaccard similarity
+//
+// Function to find images with similar words
+function findImagesWithSimilarWords(descriptions, searchTerms) {
+    const images = descriptions.split('\n');
+    const similarityThreshold = 0.5; // Adjust as needed
+
+    const similarImages = [];
+
+    images.forEach((description, index) => {
+        const descriptionWords = description.split(' ');
+        const searchWords = searchTerms.split(' ');
+
+        for (const descWord of descriptionWords) {
+            for (const searchWord of searchWords) {
+                const similarity = calculateJaccardSimilarity(descWord, searchWord);
+
+                if (similarity >= similarityThreshold) {
+                    similarImages.push(index);
+                    break; // If any word is similar, add the image and break the inner loop
+                }
+            }
+        }
+    });
+
+    return similarImages;
+}
+
+// Function to calculate Jaccard similarity
+function calculateJaccardSimilarity(text1, text2) {
+    const set1 = new Set(text1.split(','));
+    const set2 = new Set(text2.split(' '));
+    const intersectionSize = [...set1].filter(word => set2.has(word)).length;
+    const unionSize = set1.size + set2.size - intersectionSize;
+    console.log(intersectionSize,unionSize);
+    return intersectionSize / unionSize;
 }
 
 // Function to display images
-function displayImages(imageMatches) {
+function displayImages(imageIndexes) {
     const imageContainer = document.getElementById('imageContainer');
-    imageMatches.forEach((description, index) => {
+    imageIndexes.forEach(index => {
         const imageIndex = String(index).padStart(4, '0');
-        console.log(imageIndex);
-        const imagePath = 'http://127.0.0.1:8080/' + imageIndex + '.png'; // Modify this line
+        const imagePath = "http://127.0.0.1:8080/" +`${imageIndex}.png`;
         const imageElement = document.createElement('img');
         imageElement.src = imagePath;
-        imageElement.alt = description;
+        imageElement.alt = `Image ${imageIndex}`;
         imageContainer.appendChild(imageElement);
     });
 }
-
 
